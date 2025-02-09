4357aff4d0f34538d4745b17d953847f94b5c49d
devlux76 <devlux76@icloud.com>
2025-02-09 07:32:39 -0700

    Refactor extension loading logic to prioritize __$__nodeRequire if available

--- src/vs/workbench/api/node/extHostExtensionService.ts
+++ src/vs/workbench/api/node/extHostExtensionService.ts
@@ -127,7 +127,7 @@ export class ExtHostExtensionService extends AbstractExtHostExtensionService {
 			if (extensionId) {
 				performance.mark(`code/extHost/willLoadExtensionCode/${extensionId}`);
 			}
-			r = <T>(require.__$__nodeRequire ?? require /* TODO@esm drop the first */)(module.fsPath);
+			r = <T>(require.__$__nodeRequire ?? require)(module.fsPath); // Check for __$__nodeRequire and use it if available
 		} finally {
 			if (extensionId) {
 				performance.mark(`code/extHost/didLoadExtensionCode/${extensionId}`);
